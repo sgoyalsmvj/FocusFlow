80adcdbb52a5c2cfa58cb0a04b72547e16a5a88c
Akshat Gupta <guptaakshat370@gmai.com>
2024-04-30 15:17:02 +0530

    feat: Add styling responsiveness

--- Frontend/src/components/TimerNavbar.jsx
+++ Frontend/src/components/TimerNavbar.jsx
@@ -13,19 +13,19 @@ const TimerNavbar = () => {
   };
 
   return (
-    <div className={`flex justify-between items-center h-[130px] text-white`}>
+    <div className={`flex justify-between items-center h-[10vh] text-white bg-gray-850 border-b-[0.075rem] border-b-gray-100/10`}>
       <div>
         {!isDone ? (
           isActive ? (
             <button
-              className="shadow-lg rounded-lg  ml-[30px] p-2 w-[150px] bg-red-500  hover:bg-red-700"
+              className="shadow-lg rounded-lg  ml-[30px] p-2 sm:w-[150px] w-[100px] bg-red-500  hover:bg-red-700 font-semibold"
               onClick={() => setisActive(false)}
             >
               Pause
             </button>
           ) : (
             <button
-              className="shadow-lg rounded-lg ml-[30px] w-[150px] p-2 bg-green-500  hover:bg-green-700"
+              className="shadow-lg rounded-lg ml-[30px] sm:w-[150px] w-[100px] p-2 bg-green-500  hover:bg-green-700 font-semibold"
               onClick={() => setisActive(true)}
             >
               Start
@@ -34,14 +34,14 @@ const TimerNavbar = () => {
         ) : null}
       </div>
 
-      <h1 className="text-black">
+      <h1 className="text-white">
         <Timer isActive={isActive} isDone={isDone} />
       </h1>
 
       <div>
         {!isDone ? (
           <button
-            className="shadow-lg rounded-lg mr-[30px] w-[150px] p-2 bg-blue-500  hover:bg-blue-700"
+            className="shadow-lg rounded-lg mr-[30px] sm:w-[150px] w-[100px] p-2 bg-blue-500  hover:bg-blue-700 font-semibold"
             onClick={handleDone}
           >
             Task Done{" "}
