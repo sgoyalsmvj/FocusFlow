ef4022d126fa2f0dd78da7312a145fddeabad700
Akshat Gupta <guptaakshat370@gmai.com>
2024-05-27 15:03:56 +0530

    fix: Styling

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -66,13 +66,19 @@ const Register = () => {
           <div className="flex flex-row mb-2 space-x-3 w-full">
             <button
               onClick={creatorRole}
-              className="bg-gray-600 w-full p-1 font-semibold rounded-md"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
+              focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 
+              dark:bg-gray-600 dark:border-gray-600 dark:placeholder-gray-400 
+              dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 font-mono font-bold text-lg"
             >
               Creator
             </button>
             <button
               onClick={studentRole}
-              className="bg-gray-600 w-full p-1 font-semibold rounded-md"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
+              focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 
+              dark:bg-gray-600 dark:border-gray-600 dark:placeholder-gray-400 
+              dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 font-mono font-bold text-lg"
             >
               Student
             </button>
