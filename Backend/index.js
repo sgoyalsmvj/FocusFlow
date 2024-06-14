8ff0c876dfc986bd8b1d548a17bd2f9555da3a1d
sgoyalsmvj <sgoyalsmvj@gmail.com>
2024-06-14 19:40:54 +0530

    fix : port variable

--- Backend/index.js
+++ Backend/index.js
@@ -24,7 +24,7 @@ app.use('/api/creator', creatorRoutes);
 app.use('/api/student', studentRoutes);
 app.use('/api/auth', authRoutes);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   connectToMongoDB();
   console.log('Server is running on port 3000');
 })
