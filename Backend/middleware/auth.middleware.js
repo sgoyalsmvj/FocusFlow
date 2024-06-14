fc2817303862093fe1306d1196e0024794bb3238
sgoyalsmvj <sgoyalsmvj@gmail.com>
2024-06-14 20:41:55 +0530

    fix : status code

--- Backend/middleware/auth.middleware.js
+++ Backend/middleware/auth.middleware.js
@@ -1,32 +1,21 @@
 import Creator from "../models/creator.models.js";
 import Student from "../models/student.models.js";
+
 import jwt from "jsonwebtoken";
 
 export const isAuthenticated = async (req, res, next) => {
   try {
     const { token } = req.cookies;
-
     if (!token) {
       return res.status(401).json({
-        // Use 401 Unauthorized for authentication errors
-        message: "Please login first",
+        messaage: "please login first",
       });
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.creator = await Creator.findById(decoded._id);
+    req.student = await Student.findById(decoded._id);
 
-    // Assuming users are either creators or students, not both
-    const user =
-      (await Creator.findById(decoded._id)) ||
-      (await Student.findById(decoded._id));
-
-    if (!user) {
-      return res.status(404).json({
-        message: "User not found",
-      });
-    }
-
-    req.user = user; // Attach the user object to the request for use in next middleware/routes
     next();
   } catch (error) {
     return res.status(500).json({
